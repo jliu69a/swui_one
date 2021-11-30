4b0e2384edd465d94c0b4ef67413125f7505b9cc
Johnson Liu <liu_johnson@yahoo.com>
2021-11-30 11:40:31 -0500

    inital save for the project.

---
+++ MySwuiOne/MySwuiOne/MySwuiOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwuiOneApp.swift
+//  MySwuiOne
+//
+//  Created by Johnson Liu on 11/30/21.
+//
+
+import SwiftUI
+
+@main
+struct MySwuiOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
